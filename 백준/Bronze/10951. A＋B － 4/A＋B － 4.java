0deb0f2bc4adeca546189e81099ff4176c86d05d
human3452 <122500370+human3452@users.noreply.github.com>
2023-11-30 09:49:37 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 13108 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(sc.nextInt()+ sc.nextInt());
+        }
+        sc.close();
+       
+
+    }
+}
